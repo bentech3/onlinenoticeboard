eb554a66778930eba0605e3c39ab3c774d191c05
bentech3 <bentech667@gmail.com>
2026-03-09 05:01:25 -0700

    Fix index.html meta tag warning

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -69,6 +69,7 @@ if (!self.define) {
 }
 define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
 
+  importScripts("/push-sw.js");
   self.skipWaiting();
   workbox.clientsClaim();
 
@@ -82,7 +83,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ovlmdbhbsos"
+    "revision": "0.8m2thqns4to"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
